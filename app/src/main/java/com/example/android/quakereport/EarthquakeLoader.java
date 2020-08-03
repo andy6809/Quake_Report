3ff9a6c3d33e61f1beb82fbbd2da9cba11ee95b8
Andrew <andy6809@gmail.com>
2020-08-03 22:33:47 +1000

    Add AsyncTaskLoader

--- app/src/main/java/com/example/android/quakereport/EarthquakeLoader.java
+++ app/src/main/java/com/example/android/quakereport/EarthquakeLoader.java
@@ -1,4 +1,44 @@
 package com.example.android.quakereport;
 
-public class EarthquakeLoader {
+import android.content.AsyncTaskLoader;
+import android.content.Context;
+
+import java.util.List;
+
+/**
+ * Loads a list of earthquakes by using an AsyncTask to perform the
+ * network request to the given URL.
+ */
+
+public class EarthquakeLoader extends AsyncTaskLoader<List<Earthquake>> {
+
+    /** Tag for log messages */
+    private static final String LOG_TAG = EarthquakeLoader.class.getName();
+
+    /** Query URL */
+    private String mUrl;
+
+    public EarthquakeLoader(Context context, String url) {
+        super(context);
+        mUrl = url;
+    }
+
+    @Override
+    protected void onStartLoading() {
+        forceLoad();
+    }
+
+    /**
+     * This is on a background thread.
+     */
+
+    @Override
+    public List<Earthquake> loadInBackground() {
+        //Don't perform the request if there are no URLs, or the first URL is null.
+        if (mUrl == null) {
+            return null;
+        }
+
+        return QueryUtils.fetchEarthquakeData(mUrl);
+    }
 }
